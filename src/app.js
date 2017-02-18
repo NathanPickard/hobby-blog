306e978cf202a79abfa92ff6d64f86e9d81b7512
Nathan Pickard <nathan.pickard87@gmail.com>
2017-02-17 19:40:41 -0800

    Route and mock data added

--- src/app.js
+++ src/app.js
@@ -4,7 +4,15 @@ var express = require('express');
 
 var app = express();
 
-app.get()
+app.get('/', function(req, res){
+  res.send("<h1>Hey there</h1>");
+});
 
-app.listen(3000);
+app.get('/blog', function(req, res){
+  
+})
+
+app.listen(3000, function(){
+  console.log("The frontend server is running on port 3000!");
+});
 
